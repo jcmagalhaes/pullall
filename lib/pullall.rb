80750da44fd3fea9c84ac37b035bffbe8ad908dc
João Magalhães <joao@iterar.co>
2012-11-10 23:17:41 +0000

    Add multiple paths to a given group. List all groups.

--- lib/pullall.rb
+++ lib/pullall.rb
@@ -5,38 +5,75 @@ class Pullall
   extend Action
   
   SUB_COMMANDS = %w(add rm)
+  help = <<HELP
+
+  Pull all the repositories that belong to a group you created.
+
+  Usage:
+
+    pullall <group>                         # pull all the repos from <group>
+    pullall list                            # list all the groups
+    pullall add <path> -g <group>           # add <path> to <group>. If <group> doesn't exist create it
+    pullall add <path1> <path2> -g <group>  # add multiple paths to <group>
+    pullall rm <path> -g <group>            # remove <path> from <group>
+    pullall rm -g <group>                   # remove entire group of repositories
+
+HELP
+
   global_opts = Trollop::options do
-    banner "Pull all the repos in a group"
+    banner help
     stop_on SUB_COMMANDS
   end
 
   def self.run(*args)
     cmd = args.shift
     
+    # Set options for each subcommand
     cmd_opts = case cmd
-    when "add"
-      size = args.size
-      if size == 2
-        save_path(args[0], args[1])
-      else
-        Trollop::die "You must add one path to a group"
+    when "add" 
+      Trollop::options do
+        opt :group, "Group that will contain all the given paths", short: "-g", type: :string
       end
     when "rm"
       Trollop::options do
-        opt :group, "Copy twice for safety's sake", short: "-g"
+        opt :group, "Group from which all the given paths will be deleted", short: "-g", type: :string
+      end
+    end
+    
+    case cmd
+    when "add"
+      group = cmd_opts[:group]
+      if group
+        group_opt_index = args.index('-g') || args.index('--group')
+        paths = args - args[group_opt_index..group_opt_index + 1]
+        paths = get_real_paths(paths)
+        save_paths(*paths, cmd_opts[:group])
+      else  
+        Trollop::die "You must define a group name using -g option"
+      end
+    when "ls"
+      list_groups
+    when "rm"
+      group = cmd_opts[:group]
+      if group 
+        group_opt_index = args.index('-g') || args.index('--group')
+        paths = args - args[group_opt_index..group_opt_index + 1]
+        paths = get_real_paths(paths)
+        if paths.empty?
+          remove_group(group)
+        else
+          remove_paths(*paths, group)
+        end
+      else  
+        Trollop::die "You must define a group name using -g option"
       end
     else
+      # pullall <group> 
       if args.size == 0
         pull(cmd)
       else
-        Trollop::die "Unknown subcommand or invalid number of arguments"
+        Trollop::die "Unknown subcommand"
       end
     end
   end
-
-  #puts "Global options: #{global_opts.inspect}"
-  #puts "Subcommand: #{cmd.inspect}"
-  #puts "Subcommand options: #{cmd_opts.inspect}"
-  #puts "Remaining arguments: #{ARGV.inspect}"
-  #puts %x(git --git-dir #{ARGV[0]}/.git pull origin master)
 end
--- lib/pullall/actions.rb
+++ lib/pullall/actions.rb
@@ -5,32 +5,50 @@ module Action
 
   STORAGE = "#{ENV['HOME']}/.pullall"
 
-  def save_all(groups)
-    Oj.to_file(STORAGE, groups)
-    puts "Group successfully saved."
+  def get_real_paths(paths)
+    paths.collect! do |path|
+      if File.exists?(path)
+        File.realpath(path)
+      else
+        Trollop::die "The path #{path} doesn't exist"
+      end  
+    end
+    paths
   end
 
-  def save_path(path, group)
-    File.new(STORAGE, "w") if not File.exists?(STORAGE)
+  def list_groups
     groups = load_all
-    if groups
-      # Use real path to allow the use of .(current directory) as an argument
-      path = File.realpath(path)
-      if groups.has_key?(group)
-        groups[group] << path
-      else
-        groups[group] = [path]
-      end
-    else 
-      groups = { group => [path] }
+    groups.each do |group, paths|
+      puts "#{group} - #{paths}"
     end
+  end
+
+  def save_all(groups)
     json = Oj.dump(groups)
-    save_all(json)
+    Oj.to_file(STORAGE, json)
+  end
+
+  def save_paths(*paths, group)
+    groups = load_all
+    
+    # Use real path to allow the use of .(current directory) as an argument
+    if groups.has_key?(group)
+      paths.each do |path|
+        if groups[group].include?(path)
+          Trollop::die "Path #{path} already exists in group #{group}"
+        end
+      end
+      groups[group].push(*paths)
+    else
+      groups[group] = [*paths]
+    end
+    save_all(groups)
+    puts "Path successfully saved"
   end
 
   def load_paths(group)
     groups = load_all
-    if groups and groups.has_key?(group)
+    if groups.has_key?(group)
       groups[group]
     else
       Trollop::die "Group #{group} doesn't exist"
@@ -39,10 +57,40 @@ module Action
 
   def load_all
     begin
+      if not File.exists?(STORAGE)
+        # Initiate file with valid json
+        File.new(STORAGE, "w")
+        Oj.to_file(STORAGE, Oj.dump({}))
+      end
       json = Oj.load_file(STORAGE)
       Oj.load(json)
     rescue
-      nil
+      Trollop::die "~/.pullall has invalid JSON. If you changed that file then 
+                    fix it so that it has valid json. If this problem persists
+                    remove the file (rm ~/.pullall)."
+    end
+  end
+
+  def remove_group(group)
+    groups = load_all
+    deleted = groups.delete(group)
+    msg = deleted ? "Group successfully deleted" : "Group doesn't exist"
+    save_all(groups)
+    puts msg
+  end
+
+  def remove_paths(*paths, group)
+    groups = load_all
+
+    if groups.has_key?(group)
+      paths = groups[group]
+      if not paths.delete(path)
+        Trollop::die "Given path doesn't belong to group #{group}" 
+      end
+      save_all(groups)
+      puts "Path successfully removed from group #{group}"
+    else
+      Trollop::die "Group #{group} doesn't exist"
     end
   end
 
@@ -50,7 +98,7 @@ module Action
     paths = load_paths(group)
     paths.each do |path|
       puts "Pulling from #{path}:"
-      %x(git --git-dir #{group}/.git pull origin master)  
+      %x(git --git-dir #{path}/.git pull origin master)  
       puts "\n"
     end
   end
